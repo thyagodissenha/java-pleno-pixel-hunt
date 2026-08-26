691febe1c80eb658bb026018e24cee98296087ed
Thyago Taborda Dissenha <thyago.dissenha@gmail.com>
2026-08-25 22:23:03 -0300

    Make project Vercel compatible

--- app/page.tsx
+++ app/page.tsx
@@ -87,7 +87,7 @@ export default function Home() {
   useEffect(() => {
     const canvas = canvasRef.current;
     if (!canvas) return;
-    const ctx = canvas.getContext("2d");
+    const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
     if (!ctx) return;
 
     let frame = 0;
